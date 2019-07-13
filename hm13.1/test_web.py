8f21bf42502c5936d2399e6920e8eb5d7f56a294
kyklaed <4436100zak>
2019-07-13 16:36:23 +0300

    hm13.1

---
+++ hm13.1/test_web.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+desired_cap = {
+ 'browser': 'Chrome',
+ 'browser_version': '62.0',
+ 'os': 'Windows',
+ 'os_version': '10',
+ 'resolution': '1024x768',
+ 'name': 'Bstack-[Python] Sample Test'
+}
+
+driver = webdriver.Remote(
+    command_executor='http://vadim481:fVdtaj9XpsYtG6o9EzQp@hub.browserstack.com:80/wd/hub',
+    desired_capabilities=desired_cap)
+
+driver.get("http://www.google.com")
+if not "Google" in driver.title:
+    raise Exception("Unable to load google page!")
+elem = driver.find_element_by_name("q")
+elem.send_keys("BrowserStack")
+elem.submit()
+print (driver.title)
+driver.quit()
